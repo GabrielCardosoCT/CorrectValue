56e9dd64e3d69e7d9d3784c3b71ee6a218a18a8e
Gabriel Cardoso <gabrielfaria00@hotmail.com>
2024-03-01 11:50:01 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace ValorCorreto
+    {
+        class Program
+        {
+            static void Main (string[] args)
+            {
+                Console.WriteLine("Digite um valor entre os números 10 e 20:");
+                int numero1 = int.Parse(Console.ReadLine());
+
+                if (numero1 > 20) 
+                {
+                    Console.WriteLine("Número inválido!");
+                    Console.WriteLine("Seu número é maior que 20.");
+                }
+
+                else if (numero1 < 10) 
+                {
+                    Console.WriteLine("Número inválido!");
+                    Console.WriteLine("Seu número é menor que 10!");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{numero1} é um número válido!");
+                }
+            }
+        }
+    }
---
+++ README.md
@@ -0,0 +1,2 @@
+# CorrectValue
+ Distinguish if a number is more than 20 or less than 10.
